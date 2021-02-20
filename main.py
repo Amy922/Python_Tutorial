8369ad5aee85c678d309abfa525769efd8bd8793
Amy922 <amy.rose.nyman@gmail.com>
2021-02-20 11:41:12 -0500

    add new for loop

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-var = "Hello World" # This is an inline comment
-
-numbers = [1, 2, 3, 4, 5]
+for n in range(2, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals', x, '*', n//x)
+            break
+    else:
+        print(n, 'is a prime number')
